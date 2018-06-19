471069640dd0a4778676161bb4e67172f23d202c
Connor Coley <connor.coley@gmail.com>
2018-06-19 16:03:09 -0400

    New default example - impurity profile

--- rexgen_direct/rank_diff_wln/directcandranker.py
+++ rexgen_direct/rank_diff_wln/directcandranker.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
 
     directcorefinder = DirectCoreFinder()
     directcorefinder.restore()
-    react = '[C:21]([CH3:22])([CH3:23])([CH3:24])[N:25]=[C:26]=[O:27].[H-:19].[Na+:20].[O:28]1[CH2:29][CH2:30][CH2:31][CH2:32]1.[OH:1][c:2]1[cH:3][cH:4][c:5]([CH2:8][CH2:9][O:10][c:11]2[cH:12][cH:13][c:14]([CH:15]=[O:16])[cH:17][cH:18]2)[cH:6][cH:7]1'
+    react = '[CH3:26][c:27]1[cH:28][cH:29][cH:30][cH:31][cH:32]1.[Cl:18][C:19](=[O:20])[O:21][C:22]([Cl:23])([Cl:24])[Cl:25].[NH2:1][c:2]1[cH:3][cH:4][c:5]([Br:17])[c:6]2[c:10]1[O:9][C:8]([CH3:11])([C:12](=[O:13])[O:14][CH2:15][CH3:16])[CH2:7]2'
     (react, bond_preds, bond_scores, cur_att_score) = directcorefinder.predict(react)
 
     directcandranker = DirectCandRanker()
